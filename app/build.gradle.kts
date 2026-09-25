24d43e0da2c9434f08685f42b2977616fa2eb1cf
blicht74-art <blicht74@gmail.com>
2026-09-25 12:50:22 -0400

    Support passkey-only browser upload: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android { namespace="com.billlicht.progress"; compileSdk=36
- defaultConfig { applicationId="com.billlicht.progress.v2"; minSdk=34; targetSdk=35; versionCode=5; versionName="0.2.0" }
+ defaultConfig { applicationId="com.billlicht.progress.v21"; minSdk=34; targetSdk=35; versionCode=6; versionName="0.2.1" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget="17" }
 }
